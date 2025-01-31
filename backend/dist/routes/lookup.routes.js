a91a6d5889b5cc8427c3c06b64c32803cbc3650b
ajayxverma <aajayaverma88927@gmail.com>
2025-01-31 20:15:13 +0530

    Rescructured

--- backend/dist/routes/lookup.routes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { getUserList } from '../controller/lookup.controller.js';
-const lookUp = express.Router();
-lookUp.route('/domain-details').get(getUserList);
-export default lookUp;
--- backend/dist/utils/constatns.js
+++
@@ -1,2 +0,0 @@
-export const RAPID_API_HOST = 'x-rapidapi-host';
-export const RAPID_API_KEY = 'x-rapidapi-key';
